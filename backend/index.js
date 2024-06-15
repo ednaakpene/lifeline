92640749223ef592a1b8d7decf77d6786868fd44
theody85 <gyambrahtheodora.8@gmail.com>
2024-06-15 11:50:19 +0000

    fix login bugs

--- backend/index.js
+++ backend/index.js
@@ -7,17 +7,21 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 // import recipesRoutes from "./routes/recipes.routes.js";
 
-
 // load env variables
-dotenv.config({path: [".env.local"]});
+dotenv.config({ path: [".env.local"] });
 
 // create an express app
 const app = express();
 
 // apply middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 // app.use(express.static("uploads"));
@@ -27,13 +31,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
-
 // make database connection
 await mongoose.connect(process.env.MONGO_URI);
-
 
 // get the app to listen on port 4000
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log(`Express app is running on port: ${port}`);
+  console.log(`Express app is running on port: ${port}`);
 });
